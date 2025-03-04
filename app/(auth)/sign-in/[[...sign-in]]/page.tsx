8f8b68bcd97ebcac286c4d87f1e8c98b3efbcf03
MestiriJacem <mestirijacemm@gmail.com>
2025-03-04 16:33:09 +0100

    sign in page css

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,6 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn,ClerkLoaded,ClerkLoading } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react'
+import Image from 'next/image'
 
 export default function Page() {
     return (
@@ -13,10 +15,25 @@ export default function Page() {
                 </p>
 
             </div>
-            <div>
-            return <SignIn />
+            <div className='flex items-center justify-center mt-8'>
+                <ClerkLoaded>
+                <SignIn />
+                </ClerkLoaded>
+                <ClerkLoading>
+                    <Loader2 className='animate-spin text-muted-foreground'/>
+                </ClerkLoading>
+             
             </div>
        
+        </div>
+        <div className='h-full bg-blue-600 hidden lg:flex items-center justify-center'>
+            <Image
+            src='/logo.svg'
+            alt='logo'
+            height={100}
+            width={100}
+
+            />
         </div>
     
     </div>
