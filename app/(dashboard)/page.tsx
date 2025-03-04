9bbf3b9820c9ccf4d615e5196b2703569a770642
MestiriJacem <mestirijacemm@gmail.com>
2025-03-04 15:33:35 +0100

    adding middleware.ts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-   <Button>
-
-   </Button>
+<p>auth route</p>
   );
 }
